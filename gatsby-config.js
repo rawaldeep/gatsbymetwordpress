d8a004cb3c1f60ed361b985184a052db394183b3
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-05-22 10:36:05 +0200

    try and add netlify func

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
     },
       `gatsby-transformer-sharp`,
       `gatsby-plugin-sharp`,
+      'gatsby-plugin-netlify',
     {
       resolve: `gatsby-source-wordpress`,
       options: {
